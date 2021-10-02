2f2904689e24c757fe4baced5d1c04e0719d1e1f
Ting-Hao Lai <analth355@gmail.com>
2021-10-02 17:57:36 +0800

    Fix remove cookie when logout

--- plugins/auth.js
+++ plugins/auth.js
@@ -1,5 +1,8 @@
+import { removeJWT } from '~/services/jwt'
+
 export default ({ store, redirect }, inject) => {
   inject('logout', () => {
+    removeJWT();
     store.dispatch('logout');
     redirect('/login');
   })
