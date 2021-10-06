ea9e0b1bbba6a163b731b7d719286f41c1deb50b
Ting-Hao Lai <analth355@gmail.com>
2021-10-06 21:57:39 +0800

    Adding refresh token

--- store/index.js
+++ store/index.js
@@ -1,8 +1,9 @@
-import { setCookie, getCookie } from '~/services/cookie'
+import { setCookie, getCookie, eraseCookie } from '~/services/cookie'
 import { parseJwt } from '~/services/jwt'
 
 export const state = () => ({
   jwtToken: null,
+  refreshToken: null,
 });
 
 export const mutations = {
@@ -14,9 +15,21 @@ export const mutations = {
       setCookie('jwtToken', token, payload.exp * 1000);
     }
   },
+  updateRefreshToken(state, refreshToken) {
+    state.refreshToken = refreshToken;
+    if (process.client) {
+      setCookie('refreshToken', refreshToken);
+    }
+  },
   removeJWTToken(state) {
     state.jwtToken = null;
-  }
+  },
+  removeRefreshToken(state) {
+    state.refreshToken = null;
+    if (process.client) {
+      eraseCookie('refreshToken');
+    }
+  },
 };
 
 export const actions = {
@@ -26,19 +39,29 @@ export const actions = {
     if (jwtToken) {
       commit('updateJWTToken', jwtToken);
     }
+
+    const refreshToken = getCookie(req.headers.cookie, 'refreshToken');
+    if (refreshToken) {
+      commit('updateRefreshToken', refreshToken);
+    }
   },
   recordLoginInfo(context, loginData) {
     console.log('recordLoginInfo');
     console.log(loginData);
     context.commit('updateJWTToken', loginData.token);
+    context.commit('updateRefreshToken', loginData.refreshToken);
   },
   logout(context) {
     context.commit('removeJWTToken');
+    context.commit('removeRefreshToken');
   }
 };
 
 export const getters = {
   userToken(state) {
     return state.jwtToken;
+  },
+  refreshToken(state) {
+    return state.refreshToken;
   },
 };
